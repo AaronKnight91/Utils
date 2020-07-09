f12db5a547d667b80a132b42b94c825feba5df88
AaronKnight91 <a.knight14@hotmail.com>
2020-07-09 15:58:09 +0100

    Creating a master backup script that runs all three scripts

--- backup_media.sh
+++ backup_media.sh
@@ -1,30 +1,7 @@
 #!/bin/bash
 
-# Backup media files to external harddrive
+# Run all back up scripts
 
-# What to backup
-backup_file="/media/aaron/External1/Media"
-
-# Where to backup
-dest="/media/aaron/External2"
-
-# Create archive filename
-day=$(date +%Y%m%d%H%M)
-#hostname=$(hostname -s)
-archive_file=$day"_media_backup.tar.gz"
-
-# Print start status message
-echo "Backing up $backup_files to $dest/$archive_file"
-date
-echo
-
-# Backup the files using tar.
-tar -czvf $dest/$archive_file $backup_file
-
-# Print end status message.
-echo
-echo "Backup finished"
-date
-
-# Long listing of files in $dest to check file sizes.
-ls -lh $dest
+source ./backup_pictures.sh
+source ./backup_music.sh
+source ./backup_videos.sh
